cfca2b2e89b17bc33fb6c7b558aec1b333e35dc1
Ирина Кузнецова <kuzyakuzya2000@users.noreply.github.com>
2020-11-10 09:49:42 +0300

    Lab-1

---
+++ Кузнецова Ирина Васильевна/Lab-1/popup.js
@@ -0,0 +1,5 @@
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block"; }
+    function closeForm() {
+    document.getElementById("myForm").style.display = "none";} 
